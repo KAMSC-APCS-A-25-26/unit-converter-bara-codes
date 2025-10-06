45cc7c7ddf614e0491b9b21e1603d61f9ebce0a1
bara-codes <198811843+bara-codes@users.noreply.github.com>
2025-10-06 15:14:16 -0400

    Update TimeConverter.java

--- src/main/java/TimeConverter.java
+++ src/main/java/TimeConverter.java
@@ -6,24 +6,32 @@ public class TimeConverter {
         boolean running = true;
         while (running) {
             System.out.println("\nConvert time to total seconds\n");
+
             System.out.print("Enter Hours: ");
             int hours = sc.nextInt();
+
             System.out.print("Enter minutes: ");
             int minutes = sc.nextInt();
+
             System.out.print("Enter seconds: ");
-            int seconds = sc.nextInt();
+            int second = sc.nextInt();
+
             int convertedhours = hours * 3600;
             int convertedminutes = minutes * 60;
-            int total = convertedminutes + convertedhours + seconds;
+            int total = convertedminutes + convertedhours + second;
+
             System.out.println("\nTime Conversion:");
-            System.out.println("Input: " + hours + " hour, " + minutes + " minute, " + seconds + " second");
-            System.out.println("Total: " + total + " second");
+            System.out.println("Input: " + hours + " hour, " + minutes + " minute, " + second + " second");
+            System.out.println("Total seconds: " + total + " seconds");
+
             System.out.println("\nCalculation: ");
-            System.out.println(hours + " hours x 3600 = " + convertedhours + " second");
-            System.out.println(minutes + " minutes x 60 = " + convertedminutes + " second");
-            System.out.println(seconds + " second = " + seconds + " second");
-            System.out.println("Total: " + total + " second");
+            System.out.println(hours + " hours x 3600 = " + convertedhours + " seconds");
+            System.out.println(minutes + " minutes x 60 = " + convertedminutes + " seconds");
+            System.out.println(second + " seconds = " + second + " seconds");
+
+            System.out.println("Total: " + total + " seconds");
             System.out.println("");
+
             System.out.print("Do you want to convert another time? (y/n): ");
             String choice = sc.next();
             switch (choice) {
